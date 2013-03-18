85906dd0e3df960d7b4b2a201624f115e44b8009
dnikitadeshmukh <dnikitadeshmukh@gmail.com>
2013-03-18 11:54:02 +0530

    (Nikita) 1st commit with sample project.. lets change this for our purpose

---
+++ TMS/src/main/java/com/sample/controller/BaseController.java
@@ -0,0 +1,20 @@
+package com.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/")
+public class BaseController {
+
+    @RequestMapping(value = "/" , method = RequestMethod.GET)
+    public String welcome(ModelMap model){
+        model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+        return "index";
+    }
+
+   }
